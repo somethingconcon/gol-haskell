d17ca22faad7e833af5e98ad9da76864d82e5451
Conor Reedy <ofrequisiteessence@gmail.com>
2018-08-28 23:28:19 -0400

    Making a game of life because I've forgotten to do it!

---
+++ simple/app/Main.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Main where
+
+import Lib
+
+main :: IO ()
+main = putStrLn("poo")
+-- main = do
+--   b <- start
+--   n <- Next(b)

25acd29bb2e08ca4f29e37d30e9be9ee57d077cc
iamcell <creedy@geezeo.com>
2018-08-29 22:07:40 -0400

    IDK

--- app/Main.hs
+++ app/Main.hs
@@ -9,16 +9,37 @@ import Lib
 
 main :: IO ()
 main = do
-  p <- get
-  readFile getSeed
--- main = do
---   b <- start
---   n <- Next(b)
+  -- only worry about the first arg
+  [p] <- getArgs
+  -- provide a seed file to make the board
+  f <- getSeedFile p
+  -- validated file is passed to the fn that runs the game
+  _ <- runGameOfLife f
 
-getSeed :: String -> IO FilePath
-getSeed path = do 
+getSeedFile :: String -> IO FilePath
+getSeedFile path = do 
   -- using File specific location checking instead of "Path"
   b <- doesFileExist path
-  p <- if b
+  p <- if b == True
        then path
-       else error "derp!"
+       else selectSeedFile 5
+
+selectSeedFile :: Int -> IO FilePath
+selectSeedFile cnt =
+  if cnt == 0 
+  then 
+    throwError "boom" 
+  else do
+    _ <- putStrLn "provided file does exist on this filesystem. Please choose one from the list provided." 
+    f <- getLine
+    b <- doesFileExist f
+    if b == True 
+    then 
+    else selectSeedFile(cnt - 1)
+
+runGameOfLife :: FilePath -> IO ()
+runGameOfLife f = putStrLn f
+
+
+
+
